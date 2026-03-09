7ab6c6b9017106033bb95ea50e1b0f22a3bac165
Jules Goy <julesgoydev@gmail.com>
2026-03-10 00:19:18 +0100

    feat: background, hero text, hero image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { cn } from "@/app/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Jules Goy",
--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-6">
-      <div className="flex bg-accent rounded-4xl p-4">
-        <div className="">
+    <main
+      className="min-h-screen bg-gray-400 flex items-center justify-center p-6"
+      style={{
+        background:
+          "linear-gradient(49deg,rgba(252, 198, 122, 1) 23%, rgba(49, 65, 88, 1) 100%)",
+      }}
+    >
+      <div className="flex">
+        <div className="flex items-center text-slate-700 text-6xl">
           <p>
             Hey ! I&apos;m <strong>Jules Goy</strong> alias{" "}
             <strong>Witchoy</strong> a Junior Software Developper.
           </p>
         </div>
-        <div className="">
-          <Image
-            src="/placeholder_ai_slop.svg"
-            width={1000}
-            height={760}
-            alt="Placeholder ai slop image"
-            className="object-contain"
-          />
-        </div>
+        <Image
+          src="/no_background_ai_slop.svg"
+          width={1000}
+          height={760}
+          alt="Placeholder ai slop image"
+          className="w-full hidden md:block"
+        />
       </div>
-      <div></div>
     </main>
   );
 }
